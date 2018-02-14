d1799b5fd87ef25f2ed5a0856632c84ea81b7661
qizhenghu <springsun19@hotmail.com>
2018-02-13 20:59:20 -0500

    Create 653-TwoSumIV.java

---
+++ 653-TwoSumIV.java
@@ -0,0 +1,29 @@
+//Given a Binary Search Tree and a target number, 
+//return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class StoreInList {
+    public boolean findTarget(TreeNode root, int k) {
+        
+    }
+}
+
+class InOrderTraversal{
+    public boolean findTarget(TreeNode root, int k) {
+        
+    }
+}
+
+class BinarySearch{
+    public boolean findTarget(TreeNode root, int k) {
+        
+    }
+}
