d59e9f1cf13a01c75bedf8cf0b713c6c0924a40f
qizhenghu <springsun19@hotmail.com>
2018-02-19 19:35:58 -0500

    Update 653-TwoSumIV.java

--- 653-TwoSumIV.java
+++ 653-TwoSumIV.java
@@ -10,20 +10,45 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class StoreInList {
+class DFS {
+    private Set<Integer> set;
     public boolean findTarget(TreeNode root, int k) {
-        
+        set = new HashSet<Integer>();
+        return dfs(root, k);
     }
+    public boolean dfs(TreeNode root, int k){
+        if(root == null) return false;
+        if(set.contains(k-root.val)) return true;
+        
+        set.add(root.val);
+        return dfs(root.left, k) || dfs(root.right, k);
+    }    
 }
 
 class InOrderTraversal{
+    List<Integer> list;
     public boolean findTarget(TreeNode root, int k) {
-        
+        list = new ArrayList<Integer>();
+        inOrderTraversal(root);
+        int start = 0;
+        int end = list.size()-1;
+        while(start < end){
+            if(list.get(start) + list.get(end) == k) return true;
+            if(list.get(start) + list.get(end) < k) {
+                start++;
+            }else{
+                end--;
+            }
+            
+        }
+        return false;
+    }
+    
+    public void inOrderTraversal(TreeNode root){
+        if(root == null) return;
+        inOrderTraversal(root.left);
+        list.add(root.val);
+        inOrderTraversal(root.right);
     }
 }
 
-class BinarySearch{
-    public boolean findTarget(TreeNode root, int k) {
-        
-    }
-}
