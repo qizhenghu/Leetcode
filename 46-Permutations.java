07011a00373d3f33dfac7a2c7ff253536c1b65bb
qizhenghu <springsun19@hotmail.com>
2018-03-31 14:09:00 -0400

    Update 46-Permutations.java

--- 46-Permutations.java
+++ 46-Permutations.java
@@ -6,17 +6,15 @@ class Solution {
         
     }
     
-    private void backtracking (List<List<Integer>> list, List<Integer> temp, int[] nums){
-      if(temp.size() == nums.length){
-        list.add(new ArrayList<Integer>(temp));
-      }else{
-        for(int i = 0; i < nums.length; i++){
-          if(temp.contains(nums[i])) continue;
-          temp.add(nums[i]);
-          backtracking(list, temp, nums);
-          temp.remove(temp.size()-1);
+    private void backtracking(List<List<Integer>> list, List<Integer> temp, int[] nums){
+        if(temp.size() == nums.length){
+            list.add(new ArrayList<Integer>(temp));
         }
-      }  
-      
+        for(int i = 0; i < nums.length; i ++){
+            if(temp.contains(nums[i])) continue;
+            temp.add(nums[i]);
+            backtracking(list, temp, nums);
+            temp.remove(temp.size()-1);
+        }
     }
 }
